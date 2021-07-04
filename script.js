dede83684a8da856f188d4f4e4b69978b613849b
VladParomenkov <vladparomenkov@gmail.com>
2021-07-05 00:03:52 +0300

    write js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 document.querySelector('.b-1').addEventListener('click', () => {
-let data = document.querySelector('.i-1').value;
-document.querySelector('.out-1').innerHTML += data;
-
- // document.querySelector('.i-1').value = 123;
+  // let data = document.querySelector('.i-1').value;
+  // document.querySelector('.out-1').innerHTML += data;
+  document.querySelector('.i-1').value = 123;
 });
 
-document.querySelector('.b-2').addEventListener('click',()
-=> {
-    let data =+document.querySelector('.i-2').value; //4
-    document.querySelector('out-2').innerHTML +=data;
+document.querySelector('.b-2').addEventListener('click', () => {
+  let data = +document.querySelector('.i-2').value; //4 
+  document.querySelector('.out-2').innerHTML += data;
 });
-
