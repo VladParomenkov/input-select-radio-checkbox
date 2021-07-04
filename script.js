6297cf487bfe5fb0f1081666fa49d09241ff9a02
VladParomenkov <vladparomenkov@gmail.com>
2021-07-05 00:01:48 +0300

    i readed all scripts

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.querySelector('.b-1').addEventListener('click', () => {
-  document.querySelector('.i-1').value = 123;
+let data = document.querySelector('.i-1').value;
+document.querySelector('.out-1').innerHTML += data;
+
+ // document.querySelector('.i-1').value = 123;
 });
 
 document.querySelector('.b-2').addEventListener('click',()
