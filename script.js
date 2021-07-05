8379399b4e7e3d4e9acabf6dba25eca74d702733
ChocolateNestle <vladparomenkov@gmail.com>
2021-07-05 15:27:58 +0300

    i new function this kod

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ document.querySelector('.b-1').addEventListener('click', () => {
 });
 
 document.querySelector('.b-2').addEventListener('click', () => {
-  let data = +document.querySelector('.i-2').value; //4 
+  let data = +document.querySelector('.i-2').value; //4
   document.querySelector('.out-2').innerHTML += data;
+});
+
+document.querySelector('.b-3').addEventListener('click', () => {
+  let data = document.querySelector('.i-3').value;
+  document.querySelector('.out-3').innerHTML = data;
+});
+
+document.querySelector('.b-4').addEventListener('click', () => {
+  let data = document.querySelector('.i-4').value;
+  document.querySelector('.out-4').innerHTML = data;
+});
+
+document.querySelector('.b-5').addEventListener('click', () => {
+  let data = document.querySelector('.i-5').value;
+  document.querySelector('.out-5').innerHTML = data;
 });
