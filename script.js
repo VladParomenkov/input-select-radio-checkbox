8b58f548105060cccaecca351cf6f6a8ece250c1
VladParomenkov <vladparomenkov@gmail.com>
2021-07-05 23:22:48 +0300

    read script s7-s10

--- script.js
+++ script.js
@@ -19,7 +19,50 @@ document.querySelector('.b-4').addEventListener('click', () => {
   document.querySelector('.out-4').innerHTML = data;
 });
 
-document.querySelector('.b-5').addEventListener('click', () => {
+document.querySelector('.i-5').addEventListener('input', () => {
   let data = document.querySelector('.i-5').value;
   document.querySelector('.out-5').innerHTML = data;
+});
+
+document.querySelector('.b-6').addEventListener('click', () => {
+  // let data = document.querySelector('.i-6').value;
+  // if (document.querySelector('.i-6').checked) {
+  //   document.querySelector('.out-6').innerHTML = data;
+  // } else {
+  //   document.querySelector('.out-6').innerHTML = '';
+  // }
+  if (document.querySelector('.i-6').checked) {
+    document.querySelector('.i-6').checked = false;
+  } else {
+    document.querySelector('.i-6').checked = true;
+  }
+});
+
+document.querySelector('.b-7').addEventListener('click', () => {
+  // // let data = document.querySelector('.i-7').value;
+  // let radio = document.querySelectorAll('.i-7');
+  // for (let i = 0; i < radio.length; i++) {
+  //   if (radio[i].checked) {
+  //     break;
+  //   }
+  // }
+  // document.querySelector('.out-7').innerHTML = data;
+
+  let radio = document.querySelectorAll('.i-7').value;
+  radio[2].checked = true;
+});
+
+document.querySelector('.b-8').addEventListener('click', () => {
+  let data = document.querySelector('.i-8').value;
+  document.querySelector('.out-8').innerHTML = data;
+});
+
+document.querySelector('.b-9').addEventListener('click', () => {
+  let data = document.querySelector('.i-9').value;
+  document.querySelector('.out-9').innerHTML = data;
+});
+
+document.querySelector('.b-10').addEventListener('click', () => {
+  let data = document.querySelector('.s-10').value;
+  document.querySelector('.out-10').innerHTML = data;
 });
